07f53bb794066821a33022c89487bc149dbb6200
bbkmg256 <tecded@proton.me>
2026-06-09 18:05:10 -0300

    feat: Añadida URL para testeo de tokens de sesión

--- src/apps/autent/urls.py
+++ src/apps/autent/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login_form/", views.login_form_template),
-    path("login_sesion/", views.iniciar_sesion, name="iniciar_sesion"),
-    path("cargar_entidad/", views.cargar_usuario, name="cargar_usuario"),
+	path("login_form/", views.login_form_template),
+	path("login_sesion/", views.iniciar_sesion, name="iniciar_sesion"),
+	path("cargar_entidad/", views.cargar_usuario, name="cargar_usuario"),
+	path("login_token_test/", views.token_login_test),
 ]
