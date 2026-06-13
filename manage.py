33855079616785ec3f0260e245709cb4d8fbdb75
bbkmg256 <tecded@proton.me>
2026-06-13 01:48:04 -0300

    feat: Cambios menores

--- manage.py
+++ manage.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 from pathlib import Path
 
+
 def main():
-    """Run administrative tasks."""
-    # Obtiene el directorio padre de este modulo y
-    # agrega el dir. "src" al path de busqueda de modulos del proyecto.
-    root = Path(__file__).parent
-    sys.path.append(str(root / 'src'))
-    
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.core.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+	"""Run administrative tasks."""
+	# Especifica a python que lea y tenga al directorio src/
+	# como un modulo con codigo ejecutable
+	root = Path(__file__).parent
+	sys.path.append(str(root / "src"))
+
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.core.settings")
+	try:
+		from django.core.management import execute_from_command_line
+	except ImportError as exc:
+		raise ImportError(
+			"Couldn't import Django. Are you sure it's installed and "
+			"available on your PYTHONPATH environment variable? Did you "
+			"forget to activate a virtual environment?"
+		) from exc
+	execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+	main()
